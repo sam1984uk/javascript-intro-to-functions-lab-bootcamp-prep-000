9437241830d4c5e6077c63401a079f4f2ee1ca3e
sam ditson <samditson2@hotmail.co.uk>
2018-07-31 15:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ function logShout(string){ var x = string.toUpperCase(); console.log(x); }
 
 function logWhisper(string){var x = string.toLowerCase(); console.log(x);}
 
-function sayHiToGrandma(string){var lowercase = lowercase.toLowerCase() === lowercase; if(lowercase){return `$I can't herar you!`;}
+function sayHiToGrandma(string){var lowercase = lowercase.toLowerCase() === lowercase; if(lowercase){return `I can't herar you!`;}
